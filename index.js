099660594dcc7d41d7220e3b4f82fda457f85c57
ItsTobez <mrtobez08@gmail.com>
2022-01-29 09:12:53 +0000

    changes made

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 import chalk from 'chalk';
 import inquirer from 'inquirer';
 import { createSpinner } from 'nanospinner';
+import figlet from 'figlet';
 
+var score = 0;
 let name;
 const wait = (ms= 1500) => new Promise((resolve) => setTimeout(resolve, ms));
 async function welcome() {
@@ -13,13 +15,13 @@ async function welcome() {
         await wait();
 
         console.log(chalk.blue(`
-        Here are some guidlines!
-        1. Make sure to answer all of your questions with a capital letter (e.g Pluto)
-        2. If you get a question wrong, the whole quiz terminates!
+        Here are some guidlines:
+        1. All of the questions are multiple choice!
+        2. You will get your score at the end of the quiz!
         3. Have fun!`));
 
         await wait();
-        console.log (chalk.green( "Make sure you're ready, becuase the quiz is about to begin!"));
+        console.log (chalk.green( "Make sure you're ready, because the quiz is about to begin!"));
 }
 
 await welcome();
@@ -34,9 +36,10 @@ async function userName() {
     },
 });
     name = nameAns.u_name;
-    console.log(chalk.yellow(`Hey ${name}! Let's get started!`));
+    console.log(chalk.yellow(`Hey ${name}. Let's get started!`));
 }
 
+// This function chooses a random word from the array of words displayed below and returns it in the console whilst searching for the correct answer.
 const spinnerWords = [ 
     'Googling the answer...',
     'Checking messages...',
@@ -51,10 +54,10 @@ async function handleAnswer(correct) {
     await wait();
   
     if (correct) {
-      spinner.success({ text: `GADZOOKS! THAT'S RIGHT! I guess you humans are smater than you seem...` });
+    score++;
+      spinner.success({ text: `GADZOOKS! THAT'S RIGHT! I guess you humans are smarter than you seem...` });
     } else {
-      spinner.error({ text: `Ah shoot, you got that wrong! Better luck next time...` });
-      process.exit(1);
+      spinner.error({ text: `Oh no, you got that wrong! Better luck next time...` });
     }
   }
 
@@ -64,15 +67,15 @@ async function handleAnswer(correct) {
       type: 'list',
       message: 'What is the name of the planet closest to the sun?',
       choices: [
-        'Mercury',
         'Earth',
+        'Pluto',
         'Venus',
         'Mars',
         'Jupiter',
-        'Saturn',
+        'Mercury',
         'Uranus',
         'Neptune',
-        'Pluto'
+        'Saturn'
       ],
     });
 
@@ -85,10 +88,10 @@ async function q1() {
       type: 'list',
       message: 'What is the smallest planet in our Solar System?',
       choices: [
-        'Mercury',
+        'Mars',
         'Earth',
         'Venus',
-        'Mars',
+        'Mercury',
         'Jupiter',
         'Saturn',
         'Uranus',
@@ -105,8 +108,8 @@ async function q1() {
           type: 'list',
           message: 'What country is NASA from?',
           choices: [
-            'America',
             'Canada',
+            'America',
             'China',
             'India',
             'Russia',
@@ -116,12 +119,68 @@ async function q1() {
     return handleAnswer(answers.q_2 === 'America');
 };
 
+async function q3() {
+    const answers = await inquirer.prompt({
+      name: 'q_3',
+      type: 'list',
+      message: 'What is the hottest planet in our Solar System?',
+      choices: [
+        'Mercury',
+        'Earth',
+        'Venus',
+        'Mars',
+        'Jupiter',
+        'Saturn',
+        'Uranus',
+        'Neptune',
+        'Pluto'
+      ],
+    });
+
+return handleAnswer(answers.q_3 === 'Venus');
+};
+
+async function q4() {
+    const answers = await inquirer.prompt({
+      name: 'q_4',
+      type: 'list',
+      message: 'What is the coldest planet in our Solar System?',
+      choices: [
+        'Mercury',
+        'Earth',
+        'Venus',
+        'Mars',
+        'Jupiter',
+        'Saturn',
+        'Uranus',
+        'Neptune',
+        'Pluto'
+      ],
+    });
+
+return handleAnswer(answers.q_4 === 'Uranus');
+};
+
+// This is the congratulatory message at the end of the game
+
 async function winner() {
-    console.log(chalk.green(`CONGRATULATIONS ${name}! You EVERYTHING correct! I'm going to have to tell the other planets about this!`));
+    figlet ('CONGRATS!', function(err, data) {
+        if (err) {
+            console.log('UH OH, there was an error. Try again :(');
+            console.dir(err);
+            return;
+        }
+        console.log(data);
+        console.log (chalk.red(`CONGRATULATIONS, ${name}! You finished the quiz. By the way, your score was ${score}! I'm gonna have to tell the other planets about this...`));
+    });
   }
 
   await userName();
   await q0();
   await q1();
   await q2();
+  await q3();
+  await q4();
   await winner();
+
+// Built by Toby Brown. (https://www.tobyb.xyz)
