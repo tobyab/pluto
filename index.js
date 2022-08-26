90c3c5e50338712264e2e4574ede594dfcc0acb8
Toby <mrtobez08@gmail.com>
2022-08-26 07:28:50 -0700

    code update! Cleaned up, and more features added.

--- index.js
+++ index.js
@@ -2,185 +2,147 @@
 import chalk from 'chalk';
 import inquirer from 'inquirer';
 import { createSpinner } from 'nanospinner';
-import figlet from 'figlet';
+import cowsay from 'cowsay';
 
-var score = 0;
+let score = 0;
 let name;
-const wait = (ms= 1500) => new Promise((resolve) => setTimeout(resolve, ms));
-async function welcome() {
-    const title = chalk.magenta (
-        `Hi! I'm Pluto, welcome to my quiz!`);
+const wait = (ms = 1500) => new Promise((resolve) => setTimeout(resolve, ms));
 
-        console.log(title);
-        await wait();
+console.log(chalk.blue(`
+  Hey there, I'm Pluto! welcome to my quiz.
+`));
 
-        console.log(chalk.blue(`
-        Here are some guidlines:
-        1. All of the questions are multiple choice!
-        2. You will get your score at the end of the quiz!
-        3. Have fun!`));
+await wait();
+await userName();
+console.clear()
 
-        await wait();
-        console.log (chalk.green( "Make sure you're ready, because the quiz is about to begin!"));
-}
 
-await welcome();
+console.log(chalk.red(`
+  RULES
+
+  1. All of the questions are multiple choice
+  2. You will get your score at the end of the quiz
+  3. Have fun!
+`));
+
+await wait();
+console.log(chalk.blue("Make sure you're ready, because the quiz is about to begin!"));
+await wait();
 
 async function userName() {
-    const nameAns = await inquirer.prompt({
+  const nameAns = await inquirer.prompt({
     name: "u_name",
     type: "input",
-    message: "What's your name?",
+    message: "First off, we need to know your name!",
     deafult() {
-        return 'User';
+      return 'User';
     },
-});
-    name = nameAns.u_name;
-    console.log(chalk.yellow(`Hey ${name}. Let's get started!`));
+  });
+  name = nameAns.u_name;
+  console.log(chalk.blue(`Hey ${name}. Let's get started!`));
 }
 
-// This function chooses a random word from the array of words displayed below and returns it in the console whilst searching for the correct answer.
-const spinnerWords = [ 
-    'Googling the answer...',
-    'Checking messages...',
-    'Thinking about cats...',
-    'Refreshing Twitter...',
-    "Wondering what's for dinner...",
+let spinnerWords = [
+  "Trying to remember the answer...",
+  "Checking messages...",
+  "Thinking about cats...",
+  "Refreshing Twitter...",
+  "Wondering what's for dinner...",
 ]
-var wordOut = spinnerWords[Math.floor(Math.random() * spinnerWords.length)];
+
+let wordOut = spinnerWords[Math.floor(Math.random() * spinnerWords.length)];
+
+let planets = [
+  'Mars',
+  'Earth',
+  'Venus',
+  'Mercury',
+  'Jupiter',
+  'Saturn',
+  'Uranus',
+  'Neptune',
+  'Pluto'
+]
 
 async function handleAnswer(correct) {
-    const spinner = createSpinner(wordOut).start();
-    await wait();
-  
-    if (correct) {
+  const spinner = createSpinner(wordOut).start();
+  await wait();
+
+  if (correct) {
     score++;
-      spinner.success({ text: `GADZOOKS! THAT'S RIGHT! I guess you humans are smarter than you seem...` });
-    } else {
-      spinner.error({ text: `Oh no, you got that wrong! Better luck next time...` });
-    }
+    spinner.success({ text: `GADZOOKS! That's right! I guess you humans are smarter than you seem...` });
+  } else {
+    spinner.error({ text: `Oh no, that's wrong! Better luck next time.` });
   }
+}
 
-  async function q0() {
-    const answers = await inquirer.prompt({
-      name: 'q_0',
-      type: 'list',
-      message: 'What is the name of the planet closest to the sun?',
-      choices: [
-        'Earth',
-        'Pluto',
-        'Venus',
-        'Mars',
-        'Jupiter',
-        'Mercury',
-        'Uranus',
-        'Neptune',
-        'Saturn'
-      ],
-    });
-
-    return handleAnswer(answers.q_0 === 'Mercury');
+async function q0() {
+  const answers = await inquirer.prompt({
+    name: 'q0',
+    type: 'list',
+    message: '1. What is the name of the planet closest to the sun?',
+    choices: planets
+  });
+  return handleAnswer(answers.q0 === 'Mercury');
 };
 
 async function q1() {
-    const answers = await inquirer.prompt({
-      name: 'q_1',
-      type: 'list',
-      message: 'What is the smallest planet in our Solar System?',
-      choices: [
-        'Mars',
-        'Earth',
-        'Venus',
-        'Mercury',
-        'Jupiter',
-        'Saturn',
-        'Uranus',
-        'Neptune',
-        'Pluto'
-      ],
-    });
-    return handleAnswer(answers.q_1 === 'Pluto');
+  const answers = await inquirer.prompt({
+    name: 'q1',
+    type: 'list',
+    message: '2. What is the smallest planet in our Solar System?',
+    choices: planets
+  });
+  return handleAnswer(answers.q1 === 'Pluto');
 };
 
-    async function q2() {
-        const answers = await inquirer.prompt({
-          name: 'q_2',
-          type: 'list',
-          message: 'What country is NASA from?',
-          choices: [
-            'Canada',
-            'America',
-            'China',
-            'India',
-            'Russia',
-          ],
-        });
-
-    return handleAnswer(answers.q_2 === 'America');
+async function q2() {
+  const answers = await inquirer.prompt({
+    name: 'q2',
+    type: 'list',
+    message: '3. What country is NASA based in?',
+    choices: [
+      'Canada',
+      'America',
+      'China',
+      'India',
+      'England',
+    ],
+  });
+  return handleAnswer(answers.q2 === 'America');
 };
 
 async function q3() {
-    const answers = await inquirer.prompt({
-      name: 'q_3',
-      type: 'list',
-      message: 'What is the hottest planet in our Solar System?',
-      choices: [
-        'Mercury',
-        'Earth',
-        'Venus',
-        'Mars',
-        'Jupiter',
-        'Saturn',
-        'Uranus',
-        'Neptune',
-        'Pluto'
-      ],
-    });
-
-return handleAnswer(answers.q_3 === 'Venus');
+  const answers = await inquirer.prompt({
+    name: 'q3',
+    type: 'list',
+    message: '4. What is the hottest planet in our Solar System?',
+    choices: planets
+  });
+  return handleAnswer(answers.q3 === 'Venus');
 };
 
 async function q4() {
-    const answers = await inquirer.prompt({
-      name: 'q_4',
-      type: 'list',
-      message: 'What is the coldest planet in our Solar System?',
-      choices: [
-        'Mercury',
-        'Earth',
-        'Venus',
-        'Mars',
-        'Jupiter',
-        'Saturn',
-        'Uranus',
-        'Neptune',
-        'Pluto'
-      ],
-    });
-
-return handleAnswer(answers.q_4 === 'Uranus');
+  const answers = await inquirer.prompt({
+    name: 'q4',
+    type: 'list',
+    message: '5. What is the coldest planet in our Solar System?',
+    choices: planets
+  });
+  return handleAnswer(answers.q4 === 'Uranus');
 };
 
-// This is the congratulatory message at the end of the game
+async function end() {
+  console.log(chalk.blue(cowsay.say({
+    text : `CONGRATULATIONS, ${name}! your score was ${score}.`,
+    e: '^^',
+    T: 'U'
+  })));
+};
 
-async function winner() {
-    figlet ('CONGRATS!', function(err, data) {
-        if (err) {
-            console.log('UH OH, there was an error. Try again :(');
-            console.dir(err);
-            return;
-        }
-        console.log(data);
-        console.log (chalk.red(`CONGRATULATIONS, ${name}! You finished the quiz. By the way, your score was ${score}! I'm gonna have to tell the other planets about this...`));
-    });
-  }
-
-  await userName();
-  await q0();
-  await q1();
-  await q2();
-  await q3();
-  await q4();
-  await winner();
-
-// Built by Toby Brown. (https://www.tobyb.xyz)
+await q0();
+await q1();
+await q2();
+await q3();
+await q4();
+await end();
